a0b6e3f0738d80272a8c1e1d00de9dfeb5244615
ladysnazzy <ifejichinaza77@gmail.com>
2022-09-10 12:01:50 +0100

    modifed the html and css to including better styling  also modied the javascript into a simpler code and also adding simple validation to it

--- agefunction.js
+++ agefunction.js
@@ -1,37 +1,17 @@
-function ageFunction() {
-    var inputt = document.getElementById('dob').value
-    var d = new Date(inputt)
-
-    var mont_dif = Date.now() - d.getTime()
-    var curnt_age = new Date(mont_dif)
-    var year = curnt_age.getFullYear()
-    var age = Math.abs(year - 1970)
-
-    alert(document.getElementById('output').innerHTML =  "YOU ARE " + age + " YEARS OLD" )
-   return true
-}
+var btn = document.getElementById('btn').addEventListener('click', function(){
+    var input = document.getElementById('inp')
 
 
- // document.getElementById('output').addEventListener ('click', function(){
-    //     alert(document.getElementById('output').innerHTML = age)
-    
-    // })     
-
-
-// function ageFunction(){
-//     var inp = document.getElementById('dob').value
-//     var d = new Date(inp)
-
-//     var moth_diff = Date.now() - d.getTime()
-//     var curnt_age = new Date(moth_diff)
-//     var year = curnt_age.getFullYear
-//     var age = Math.abs(year - 1970)
-
-//     alert(document.getElementById('output').innerHTML =  "YOU ARE: " + age + " YEARS OLD" )
-//    return true
-
-// }
-
-
-
-// // console.log(ageFunction())
+    var inputt = input.value 
+    if(inputt == ''){
+        input.style.border = '1px solid red'
+        document.getElementById('inp_err').innerHTML = 'please enter age'
+        return false
+    }else{
+        var d = new Date()
+        var dd = d.getFullYear()
+        var result = dd - Number(inputt)
+        document.getElementById('output').innerHTML = result
+        document.getElementById('inp_err').innerHTML = ''
+    }
+})
